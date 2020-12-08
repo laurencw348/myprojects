947895dd9137a8abf34a4ee87f401544658fc98b
Lauren Crosbie-Walsh <lmc348@cornell.edu>
2020-12-08 16:26:52 +0800

    valid

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,47 +1,38 @@
-// When form is submitted:
 
 $("#request-form").on("submit", function() {
   var formValid = true;
 
-  console.log("form submitted");
 
-  //if name data is valid:
-  //  hide name feedback
-  //else:
-  //  show name feedback
+  if( $("#name").prop("validity").valid ) {
+    $("#name-feedback").addClass("hidden");
 
-  //if email is valid:
-  //  hide email feedback
-  //else:
-  //  show email feedback
-
-  if( $("#email").prop("validity").valid ) {
-
-    console.log("hide feedback");
-    $("#feedback-email").addClass("hidden");
-
-    // TODO: snippet(s) for if component data is valid
 
   } else {
-    console.log("show feedback")
-    $("#feedback-email").removeClass("hidden");
-
+    $("#name-feedback").removeClass("hidden");
 
     formValid = false;
   }
 
-  //if requested subject title data is valid:
-  //  hide requested subject title feedback
-  //else:
-  //  show requested subject title feedback
+  if( $("#email").prop("validity").valid ) {
+    $("#email-feedback").addClass("hidden");
 
 
+  } else {
+    $("#email-feedback").removeClass("hidden");
+
+    formValid = false;
+  }
+
+  if( $("#title").prop("validity").valid ) {
+    $("#title-feedback").addClass("hidden");
 
 
+  } else {
+    $("#title-feedback").removeClass("hidden");
 
-  //if form data is valid:
-  //  send data to server
-  //else:
-  //  prevent form from sending data to server//
+    formValid = false;
+  }
+
+
   return formValid;
 });
